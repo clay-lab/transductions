ec7e9031f32a61f16ef1b2c05c426e9678b67a6c
jopetty <jacksonopetty@gmail.com>
2020-06-24 18:53:06 -0600

    Add separated training and testing modules

---
+++ test.py
@@ -0,0 +1,3 @@
+# test.py
+# 
+# Code for testing a trained model on provided datasets.
---
+++ train.py
@@ -0,0 +1,3 @@
+# train.py
+# 
+# Code for training sequence-to-sequence (Seq2Seq) models on provided datasets.
