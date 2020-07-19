7b0d5da08e1ae7b47076d81bc8666422aa14001d
jopetty <jacksonopetty@gmail.com>
2020-07-19 16:55:08 -0600

    Add sbatcher script

---
+++ batcher.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+
+## Usage:
+#       ./basher.sh EXP_DIR TASK ENC DEC ATTN
+
+## Values
+EXPDIR=$1
+TASK=$2
+ENC=$3
+DEC=$4
+ATTN=$5
+
+MAIL='jackson.petty@yale.edu'
+EXPPATH="$EXPDIR/$TASK/$ENC-$DEC-$ATTN" 
+NUM=$(find $EXPPATH/* -maxdepth 0 -type d | wc -l | tr -d '[:space:]')
+
+cat > "$TASK-$ENC-$DEC-$ATTN.sh" << EOF1
+#! /usr/bin/env bash
+#SBATCH --job-name = "$TASK-$ENC-$DEC-$ATTN-$NUM"
+#SBATCH --ntasks=1 --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --time = 1:00:00
+#SBATCH --mail-type=FAIL, END
+#SBATCH --mail-user="$MAIL"
+#SBATCH --output="$TASK-$ENC-$DEC-$ATTN-$NUM.out"
+
+export PATH=\$HOME/anaconda3/bin:\$PATH
+python main.py train -t "$TASK" -a $ATTN -E $EXPDIR -e $ENC -d $DEC
+EOF1
+
